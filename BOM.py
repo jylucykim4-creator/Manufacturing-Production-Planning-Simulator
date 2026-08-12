5114acbf5d15e3a03b92ef18153d31263f363c1e
jylucykim4-creator <jylucykim4@gmail.com>
2026-08-12 19:14:03 +0800

    BOM.py

--- BOM.py
+++ BOM.py
@@ -1,10 +1,18 @@
+# Bill of Materials (BOM) for an iClicker-inspired classroom response device
+
 bom = {
-    "Bicycle": {
-        "Frame": 1,
-        "Wheel": 2,
-        "Handlebar": 1,
-        "Seat": 1
+    "Response Device": {
+        "Plastic Housing": 1,
+        "PCB": 1,
+        "Microcontroller": 1,
+        "RF Module": 1,
+        "LCD Display": 1,
+        "Button": 5,
+        "Battery": 2,
+        "Battery Contact": 2
     }
 }
 
-print(bom)
+print("Bill of Materials:")
+for component, quantity in bom["Response Device"].items():
+    print(f"{component}: {quantity}")
